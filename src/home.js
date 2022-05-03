50e818fcd24e0f07e56b735b620ff6bbbbad3ec9
Jim Jenkinson <jamesjenkinson10@gmail.com>
2022-05-03 03:54:27 +0100

    Commit before adding css
    Because i will probably break something

---
+++ src/home.js
@@ -0,0 +1,7 @@
+function buildHomePage() {
+  const home = document.createElement("div");
+
+  const restaurantImage = document.createElement("image");
+  restaurantImage.src = "src\images\favour-otunji-dD7xFyDzES4-unsplash.jpg";
+  //Photo by <a href="https://unsplash.com/@kodeblacc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Favour Otunji</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+}
---
+++ src/site.js
@@ -0,0 +1,77 @@
+//header, contains navbar
+function buildHeader() {
+  const header = document.createElement("header");
+  const name = document.createElement("h1");
+  name.textContent = "Restaurant";
+  header.appendChild(name);
+  header.appendChild(buildNavBar());
+
+  return header;
+}
+
+//nav bar, creates a 3 button nav bar
+function buildNavBar() {
+  const navBar = document.createElement("navBar");
+
+  const homeBtn = document.createElement("button");
+  homeBtn.textContent = "Home";
+  homeBtn.addEventListener("click", (e) => {
+    console.log("home");
+  });
+
+  const menuBtn = document.createElement("button");
+  menuBtn.textContent = "Menu";
+  menuBtn.addEventListener("click", (e) => {
+    console.log("menu");
+  });
+
+  const contactBtn = document.createElement("button");
+  contactBtn.textContent = "Contact Us";
+  contactBtn.addEventListener("click", (e) => {
+    console.log("contact");
+  });
+
+  navBar.appendChild(homeBtn);
+  navBar.appendChild(menuBtn);
+  navBar.appendChild(contactBtn);
+
+  return navBar;
+}
+//main content
+function buildMain() {
+  const main = document.createElement("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+//footer
+function buildFooter() {
+  const footer = document.createElement("footer");
+
+  const author = document.createElement("p");
+  author.textContent = "Created by Jim Jenkinson ";
+
+  const ghIcon = document.createElement("i");
+  ghIcon.className = "fa-brands fa-github";
+
+  const ghLink = document.createElement("a");
+  ghLink.href = "https://superjim.github.io/";
+
+  author.appendChild(ghIcon);
+  ghLink.appendChild(author);
+  footer.appendChild(ghLink);
+
+  return footer;
+}
+
+//build website
+
+function buildWebsite() {
+  const content = document.getElementById("content");
+
+  content.appendChild(buildHeader());
+  content.appendChild(buildMain());
+  content.appendChild(buildFooter());
+}
+
+export default buildWebsite;
